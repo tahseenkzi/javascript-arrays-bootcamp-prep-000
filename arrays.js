51b19340b38380e98a31e0020afbaa467fa0da4c
Tahseen Kazi <myronintro@gmail.com>
2018-08-30 04:32:23 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -3,7 +3,7 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
 
 function addElementToBeginningOfArray(array, element){
-  array.unshift(element);
+  return [element, ... array];
 }
 
 
